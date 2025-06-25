efe44b226d2cca701f085fcf222f761f621f4172
peter.kocsis <kopemi86@gmail.com>
2025-06-25 14:33:14 +0200

    feat(availability-editor):Move availability logic from space-manager component to the dedicated availablity-editor component

--- personal-planner/src/app/space-manager/availability-editor/availability-editor.component.ts
+++ personal-planner/src/app/space-manager/availability-editor/availability-editor.component.ts
@@ -1,11 +1,110 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { DailyTimeSlotEditorComponent, TimeSlot } from '../daily-time-slot-editor/daily-time-slot-editor.component';
 
 @Component({
   selector: 'app-availability-editor',
-  imports: [],
+  imports: [DailyTimeSlotEditorComponent],
   templateUrl: './availability-editor.component.html',
   styleUrl: './availability-editor.component.scss'
 })
 export class AvailabilityEditorComponent {
+
+  days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.ranges.update((old)=> {return {
+        Thursday: [
+          {
+            start: {
+              hour: 6,
+              minutes: 0,
+            },
+            end: { hour: 8, minutes: 0 },
+          },
+          {
+            start: {
+              hour: 10,
+              minutes: 0,
+            },
+            end: { hour: 20, minutes: 0 },
+          },
+        ],
+        Saturday: [
+          {
+            start: {
+              hour: 8,
+              minutes: 0,
+            },
+            end: { hour: 10, minutes: 0 },
+          },
+          {
+            start: {
+              hour: 12,
+              minutes: 0,
+            },
+            end: { hour: 15, minutes: 0 },
+          },
+          {
+            start: {
+              hour: 18,
+              minutes: 0,
+            },
+            end: { hour: 22, minutes: 0 },
+          },
+        ],
+      }});
+    }, 10000);
+  }
+
+  ranges = signal<{ [key: string]: TimeSlot[] }>({
+    Monday: [
+      {
+        start: {
+          hour: 6,
+          minutes: 0,
+        },
+        end: { hour: 8, minutes: 0 },
+      },
+      {
+        start: {
+          hour: 10,
+          minutes: 0,
+        },
+        end: { hour: 20, minutes: 0 },
+      },
+    ],
+    Tuesday: [
+      {
+        start: {
+          hour: 8,
+          minutes: 0,
+        },
+        end: { hour: 10, minutes: 0 },
+      },
+      {
+        start: {
+          hour: 12,
+          minutes: 0,
+        },
+        end: { hour: 15, minutes: 0 },
+      },
+      {
+        start: {
+          hour: 18,
+          minutes: 0,
+        },
+        end: { hour: 22, minutes: 0 },
+      },
+    ],
+  });
 
 }
--- personal-planner/src/app/space-manager/space-manager.component.ts
+++ personal-planner/src/app/space-manager/space-manager.component.ts
@@ -2,12 +2,9 @@ import {
   Component,
   ElementRef,
   inject,
-  OnInit,
-  signal,
   viewChildren,
 } from '@angular/core';
 import {
-  CdkDragDrop,
   DragDropModule,
   moveItemInArray,
 } from '@angular/cdk/drag-drop';
@@ -15,17 +12,13 @@ import { SpacesService } from '../../adapters/spaces.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
-import { ISpace } from '../interfaces/space.interface';
 import { MatButton, MatButtonModule } from '@angular/material/button';
 import { SpaceItemComponent } from './space-item/space-item.component';
 import { AppStateService } from '../../services/app-state.service';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
-import { CommonModule, Time } from '@angular/common';
-import {
-  DailyTimeSlotEditorComponent,
-  TimeSlot,
-} from './daily-time-slot-editor/daily-time-slot-editor.component';
+import { CommonModule } from '@angular/common';
+import { AvailabilityEditorComponent } from './availability-editor/availability-editor.component';
 
 @Component({
   selector: 'app-space-manager',
@@ -40,7 +33,7 @@ import {
     MatIconModule,
     CommonModule,
     MatButtonModule,
-    DailyTimeSlotEditorComponent,
+    AvailabilityEditorComponent
   ],
   templateUrl: './space-manager.component.html',
   styleUrl: './space-manager.component.scss',
@@ -52,104 +45,6 @@ export class SpaceManagerComponent {
 
   spaces = this.appStateService.spaces;
   newSpaceName: string = '';
-
-  days = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday',
-  ];
-
-  constructor() {
-    setTimeout(() => {
-      this.ranges.update((old)=> {return {
-        Thursday: [
-          {
-            start: {
-              hour: 6,
-              minutes: 0,
-            },
-            end: { hour: 8, minutes: 0 },
-          },
-          {
-            start: {
-              hour: 10,
-              minutes: 0,
-            },
-            end: { hour: 20, minutes: 0 },
-          },
-        ],
-        Saturday: [
-          {
-            start: {
-              hour: 8,
-              minutes: 0,
-            },
-            end: { hour: 10, minutes: 0 },
-          },
-          {
-            start: {
-              hour: 12,
-              minutes: 0,
-            },
-            end: { hour: 15, minutes: 0 },
-          },
-          {
-            start: {
-              hour: 18,
-              minutes: 0,
-            },
-            end: { hour: 22, minutes: 0 },
-          },
-        ],
-      }});
-    }, 10000);
-  }
-
-  ranges = signal<{ [key: string]: TimeSlot[] }>({
-    Monday: [
-      {
-        start: {
-          hour: 6,
-          minutes: 0,
-        },
-        end: { hour: 8, minutes: 0 },
-      },
-      {
-        start: {
-          hour: 10,
-          minutes: 0,
-        },
-        end: { hour: 20, minutes: 0 },
-      },
-    ],
-    Tuesday: [
-      {
-        start: {
-          hour: 8,
-          minutes: 0,
-        },
-        end: { hour: 10, minutes: 0 },
-      },
-      {
-        start: {
-          hour: 12,
-          minutes: 0,
-        },
-        end: { hour: 15, minutes: 0 },
-      },
-      {
-        start: {
-          hour: 18,
-          minutes: 0,
-        },
-        end: { hour: 22, minutes: 0 },
-      },
-    ],
-  });
 
   drop(event: any) {
     const spaceCopy = [...this.spaces()];
