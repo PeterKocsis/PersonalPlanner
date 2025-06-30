abf148c343e13d0092a2f539a39b7b7b3d17829c
peter.kocsis <kopemi86@gmail.com>
2025-06-30 11:23:00 +0200

    feat(availability-editor): enhance availability editor with planned time input and toggle for unavailable days

--- personal-planner/src/app/space-manager/availability-editor/availability-editor.component.ts
+++ personal-planner/src/app/space-manager/availability-editor/availability-editor.component.ts
@@ -4,10 +4,22 @@ import {
   ITimeFrameAvailabilityTemplate,
   ITimeSlot,
 } from '../../interfaces/time-slot';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-availability-editor',
-  imports: [DailyTimeSlotEditorComponent],
+  imports: [
+    DailyTimeSlotEditorComponent,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './availability-editor.component.html',
   styleUrl: './availability-editor.component.scss',
 })
@@ -41,8 +53,13 @@ export class AvailabilityEditorComponent {
   });
 
   countAvailableDays = computed(() => {
-    return this.showUnavailableDays() ? 7 : this.dailyAvailabilities().filter((availability) => availability.isAvailable).length;
+    return this.showUnavailableDays()
+      ? 7
+      : this.dailyAvailabilities().filter(
+          (availability) => availability.isAvailable
+        ).length;
   });
+  plannedTimePortion: number = 0.5;
 
   onTimeSlotChanged(changedDayTimeSlots: ITimeSlot[], dayIndex: number) {
     this.timeFrameAvailability.update((old) => {
@@ -63,9 +80,33 @@ export class AvailabilityEditorComponent {
         ...old,
         dailyAvailabilities: old.dailyAvailabilities.map(
           (availability, index) =>
-            index === dayIndex ? { ...availability, isAvailable: $event } : availability
+            index === dayIndex
+              ? { ...availability, isAvailable: $event }
+              : availability
         ),
       };
     });
+  }
+
+  totalAvailableTime = computed((): number => {
+    return this.dailyAvailabilities().reduce((total, availability) => {
+      if (availability.isAvailable) {
+        return (
+          total +
+          availability.timeSlots.reduce(
+            (dayTotal, slot) =>
+              dayTotal +
+              (slot.end.hour - slot.start.hour) * 60 +
+              (slot.end.minutes - slot.start.minutes),
+            0
+          )
+        );
+      }
+      return total;
+    }, 0);
+  });
+
+  toggleDisplayUnavailableDays() {
+    this.showUnavailableDays.update((current) => !current);
   }
 }
